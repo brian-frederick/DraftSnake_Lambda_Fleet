6ab5f1a0bd2a80706ca94a3ac58790c4302a1713
DESKTOP-CTN1NPE\brian <briandfrederick@gmail.com>
2019-03-31 14:06:27 -0500

    write player to dynamodb

--- OnCreatePlayer/OnCreatePlayer/OnCreatePlayer/Function.cs
+++ OnCreatePlayer/OnCreatePlayer/OnCreatePlayer/Function.cs
@@ -11,6 +11,7 @@ using Amazon.Lambda.APIGatewayEvents;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -31,14 +32,17 @@ namespace OnCreatePlayer
 
                 context.Logger.LogLine($"ConnectionId: {connectionId}");
 
+                var newPlayer = MapRequestToPlayer(request);
+
                 var ddbRequest = new PutItemRequest
                 {
                     TableName = "DraftSnake_Players",
                     Item = new Dictionary<string, AttributeValue>
                     {
-                        { "ConnectionId", new AttributeValue{ S = connectionId }},
-                        { "DraftId", new AttributeValue{ S = "a" }},
-                        { "IsConnected", new AttributeValue{ BOOL = true } }
+                        { "DraftId", new AttributeValue{ S = newPlayer.DraftId }},
+                        { "Name", new AttributeValue{S = newPlayer.Name} },
+                        { "ConnectionId", new AttributeValue{ S = newPlayer.ConnectionId }},
+                        { "IsConnected", new AttributeValue{ BOOL = newPlayer.IsConnected } }
                     }
                 };
 
@@ -61,7 +65,29 @@ namespace OnCreatePlayer
                 };
             }
         }
+        public Player MapRequestToPlayer(APIGatewayProxyRequest request)
+        {
+            var body = JsonConvert.DeserializeObject<JObject>(request.Body);
+            var draftId = body["DraftId"]?.ToString();
+            var name = body["Name"]?.ToString();
 
+            // TODO: Validate draft is active and Name is not a duplicate
 
+            return new Player()
+            {
+                DraftId = draftId,
+                Name = name,
+                ConnectionId = request.RequestContext.ConnectionId,
+                IsConnected = true
+            };
+        }
+
+        public class Player
+        {
+            public string DraftId { get; set; }
+            public string Name { get; set; }
+            public string ConnectionId { get; set; }
+            public Boolean IsConnected { get; set; }
+        }
     }
 }
